68ee291691e23b0bcfaff318ac9d07f3b89ac062
lakshman <diwaakartg@gmail.com>
2017-08-15 22:32:16 +0900

    ES6fied and handled edge-case

--- handler.js
+++ handler.js
@@ -1,34 +1,33 @@
 /* eslint-disable global-require, import/first, no-unused-expressions, no-console */
 if (!global._babelPolyfill) require('babel-polyfill');
 
-var aws = require('aws-sdk');
+import AWS from 'aws-sdk';
 
-var lambda = new aws.Lambda({
+const lambda = new AWS.Lambda({
     region: 'ap-northeast-1'
 });
 
 module.exports.lambdaA = (event, context, callback) => {
-  console.log("entered the function lambdaA");
 
   lambda.invoke({
     FunctionName: 'invoke-lambda-dev-lambdaB',
     InvocationType: 'RequestResponse',
-    Payload: '{ "name" : "Alex" }'
-  }, function(error, data) {
+    Payload: '{ "name" : "Toby" }'
+  }, (error, data) => {
     if (error) {
-      console.log("in error", error);
+      console.log("in error: ", error);
       callback(error);
-    }
-    if (data.Payload) {
-      console.log("in success", data.Payload);
+    } else if (data.Payload) {
+      console.log("in success: ", data.Payload);
       callback(null, data.Payload);
+    } else {
+      callback(null, data);      
     }
   });
-
 };
 
 module.exports.lambdaB = (event, context, callback) => {
   console.log("entered the function lambdaB");
   console.log('Lambda B Received event:', JSON.stringify(event, null, 2));
-  context.succeed('Hello ' + event.name);
+  callback(null, 'Hello ' + event.name);
 };
